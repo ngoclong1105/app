50ac496c815125218cd479b982b452b15cfdd89a
ngoclong1105 <bighead19980@gmail.com>
2020-02-14 22:38:14 +0700

    app

--- endpython/settings.py
+++ endpython/settings.py
@@ -80,15 +80,10 @@ WSGI_APPLICATION = 'endpython.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'comic',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
